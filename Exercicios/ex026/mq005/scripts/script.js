094ae4d17eab05198d5dff9dec9e973995f31fd3
jonathas.silvaa <jonathassilvaaraujo4@gmail.com>
2025-10-26 12:53:52 -0300

    ex026 atualizado

--- Exercicios/ex026/mq005/scripts/script.js
+++ Exercicios/ex026/mq005/scripts/script.js
@@ -1,7 +1,17 @@
+
+const navMenu = document.getElementById("navMenu");
 function clickMenu(){
     if(navMenu.style.display == "block"){
         navMenu.style.display = "none"
     }else{
         navMenu.style.display = "block"
+    }
+}
+
+function mudouTamanho(){
+    if(window.innerWidth >= 768){
+        navMenu.style.display = "block";
+    }else{
+        navMenu.style.display = "none";
     }
 }
